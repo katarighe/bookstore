9a4763ead86dc9b7e084121807f981564b7c0ca5
katarighe <m.ighe@hotmail.com>
2023-08-17 16:48:15 +0300

    Create the Add Book JS function

---
+++ src/components/AddBook.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AddBook = () => (
+  <div className="form">
+    <h2>Add New Book</h2>
+    <form>
+      <input type="text" name="title" placeholder="Title" />
+      <input type="text" name="author" placeholder="Author" />
+      <button type="submit" id="add-book">ADD BOOK</button>
+    </form>
+  </div>
+);
+
+export default AddBook;
