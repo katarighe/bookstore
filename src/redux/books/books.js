b2bb0cd2aca01294d5f48ce6b48613152d4448a8
katarighe <m.ighe@hotmail.com>
2023-08-21 18:29:49 +0300

    Fix styling line errors

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,31 +2,32 @@ const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 const initalState = [];
 const booksReducer = (state = initalState, action) => {
-    switch(action.type) {
-        case 'ADD_BOOK':
-        return [...state, 
-            {
-                id: action.id,
-                title: action.payload.title,
-                author: action.payload.author,
-            },
-        ];
+  switch (action.type) {
+    case 'ADD_BOOK':
+      return [
+        ...state,
+        {
+          id: action.id,
+          title: action.payload.title,
+          author: action.payload.author,
+        },
+      ];
 
-        case 'REMOVE_BOOKS':
-            return state.filter((book) => book.id !== action.id);
-            default:
-                return state;
-    }
+    case 'REMOVE_BOOKS':
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
 };
 
 export const addAction = (newBook) => ({
-    type: ADD_BOOK,
-    payload: newBook,
+  type: ADD_BOOK,
+  payload: newBook,
 });
 
 export const removeAction = (id) => ({
-    type: REMOVE_BOOK,
-    id,
+  type: REMOVE_BOOK,
+  id,
 });
 
 export default booksReducer;
