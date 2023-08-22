dcfaff4b0ac65a27b079f9f7a1c9e6e7dbd270fa
katarighe <m.ighe@hotmail.com>
2023-08-22 14:00:01 +0300

    Fix quote-related errors

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -5,39 +5,38 @@ const AddBook = ({ AddNewBook }) => {
   const [NewBook, setNewBook] = useState({ title: '', author: '' });
 
   return (
-    <div id='form_container'>
+    <div id="form_container">
       <h2>Add New Book</h2>
       <form
         onSubmit={(e) => {
           AddNewBook(e, NewBook);
-          setNewBook({ title: "", author: "" });
+          setNewBook({ title: '', author: '' });
         }}
       >
         <input
-          type='text'
-          name='title'
-          placeholder='Title'
+          type="text"
+          name="title"
+          placeholder="Title"
           value={NewBook.title}
           required
           onChange={(e) => setNewBook({ ...NewBook, title: e.target.value })}
         />
         <input
-          type='text'
-          name='author'
-          placeholder='Author'
+          type="text"
+          name="author"
+          placeholder="Author"
           value={NewBook.author}
           required
           onChange={(e) => setNewBook({ ...NewBook, author: e.target.value })}
         />
-        <button type='submit' id='add_book'>
+        <button type="submit" id="add_book">
           Add Book
         </button>
       </form>
     </div>
   );
 };
-
-AddBook.PropTypes = {
+AddBook.propTypes = {
   AddNewBook: PropTypes.func.isRequired,
 };
 
