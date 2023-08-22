321fe646961abe85a6d7f9d7e3670178dae0fc61
katarighe <m.ighe@hotmail.com>
2023-08-22 15:50:37 +0300

    Create react components for Nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import { React } from 'react';
 import { Link } from 'react-router-dom';
 
-function Nav() {
+const Nav = () => {
+  const links = [
+    { id: 1, path: '/', text: 'Books' },
+    { id: 2, path: '/categories', text: 'Categories' },
+  ];
+
   return (
     <div className="nav_container">
       <ul className="nav_links">
         <h2>Bookstore CMS</h2>
-        <Link to="/">
-          <li>BOOKS</li>
-        </Link>
-        <Link to="Categories">
-          <li>CATEGORIES</li>
-        </Link>
+        {links.map((link) => (
+          <li key={link.id}>
+            <Link to={link.path}>
+              {' '}
+              {link.text}
+              {' '}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
-}
+};
 
 export default Nav;
