041f4613f667f3a779a2fa55243b92a5ca1e98dd
katarighe <m.ighe@hotmail.com>
2023-08-22 17:31:30 +0300

    Add note

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const store = configureStore();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* Provide the Redux store to the entire application */}
     <Provider store={store}>
       <App />
     </Provider>
