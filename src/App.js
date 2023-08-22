ba9e71df59905b4b767cf0f3859742a342b5d541
katarighe <m.ighe@hotmail.com>
2023-08-22 12:27:33 +0300

    Make arrangements inside the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav';
-import Books from './components/Books';
-import Categories from './components/Categories';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Nav from './components/Nav';
+import Books from './components/BookDetails';
+import Categories from './components/Categories';
 
 function App() {
   return (
