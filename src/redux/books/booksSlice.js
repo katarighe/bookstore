74d1240577c66f067f780534444c68fa60f715f6
katarighe <m.ighe@hotmail.com>
2023-08-22 12:54:10 +0300

    Add a list of at least 5 books

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      id: 1,
+      title: "The Hitchhiker's Guide to the Galaxy",
+      author: "Douglas Adams",
+      category: "Science fiction",
+    },
+    {
+      id: 2,
+      title: "The Lord of the Rings",
+      author: "J.R.R. Tolkien",
+      category: "Fantasy",
+    },
+    {
+      id: 3,
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      category: "Literary fiction",
+    },
+    {
+      id: 4,
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      category: "Classics",
+    },
+    {
+      id: 5,
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      category: "Romance",
+    },
+  ],
 };
 
 const booksSlice = createSlice({
