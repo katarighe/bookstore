325b45de71e4199fc4bb41c9a040fad77b8bc0bf
katarighe <m.ighe@hotmail.com>
2023-08-22 13:51:10 +0300

    Add PropTypes

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,4 +1,5 @@
 import { React, useState } from 'react';
+import { PropTypes } from 'prop-types';
 
 const AddBook = ({ AddNewBook }) => {
   const [NewBook, setNewBook] = useState({ title: '', author: '' });
