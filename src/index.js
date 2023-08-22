41cee0e0ef780d87da7acc3abb7ee4be32308662
katarighe <m.ighe@hotmail.com>
2023-08-22 18:39:50 +0300

    Update the changes and remove uneeded tag

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import configureStore from './redux/configureStore';
+import { store } from './redux/store';
 import App from './App';
-import './index.css';
-
-const store = configureStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Provide the Redux store to the entire application */}
     <Provider store={store}>
       <App />
     </Provider>
