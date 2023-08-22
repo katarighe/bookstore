3935f1f1166716177deb3b85381688eedbbf883d
katarighe <m.ighe@hotmail.com>
2023-08-22 13:48:57 +0300

    Add the Add Book function and its forms

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import { React, useState } from 'react';
 
-const AddBook = () => (
-  <div className="form">
-    <h2>Add New Book</h2>
-    <form>
-      <input type="text" name="title" placeholder="Title" />
-      <input type="text" name="author" placeholder="Author" />
-      <button type="submit" id="add-book">ADD BOOK</button>
-    </form>
-  </div>
-);
+const AddBook = ({ AddNewBook }) => {
+  const [NewBook, setNewBook] = useState({ title: '', author: '' });
+
+  return (
+    <div id='form_container'>
+      <h2>Add New Book</h2>
+      <form
+        onSubmit={(e) => {
+          AddNewBook(e, NewBook);
+          setNewBook({ title: "", author: "" });
+        }}
+      >
+        <input
+          type='text'
+          name='title'
+          placeholder='Title'
+          value={NewBook.title}
+          required
+          onChange={(e) => setNewBook({ ...NewBook, title: e.target.value })}
+        />
+        <input
+          type='text'
+          name='author'
+          placeholder='Author'
+          value={NewBook.author}
+          required
+          onChange={(e) => setNewBook({ ...NewBook, author: e.target.value })}
+        />
+        <button type='submit' id='add_book'>
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
+
+AddBook.PropTypes = {
+  AddNewBook: PropTypes.func.isRequired,
+};
 
 export default AddBook;
