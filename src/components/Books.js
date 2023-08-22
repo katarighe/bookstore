7e51f22eb7794b3d89a168a231e3cb8e0a18f715
katarighe <m.ighe@hotmail.com>
2023-08-22 14:12:16 +0300

    Add Redux for React components in Books.js

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addAction, removeAction } from '../redux/books/booksSlice';
 import AddBook from './AddBook';
 import Book from './Book';
 
-const Books = () => (
-  <>
-    <Book title="Book title" author="Book author" />
-    <button type="button">Remove</button>
-    <AddBook />
-  </>
-);
+const Books = () => {
+  const dispatch = useDispatch();
+  const StoredBook = useSelector((state) => state.booksReducer);
+
+  const AddNewBook = (e, NewBook) => {
+    e.preventDefault();
+    dispatch(addAction(NewBook));
+  };
+  const RemoveExistingBook = (id) => {
+    dispatch(removeAction(id));
+  };
+  return (
+    <>
+    {StoredBook.map(item => (
+      <Book title={item.title} item={item.id} author={item.author} id={item.id} RemoveExistingBook={RemoveExistingBook}/>
+    ))}
+    <AddNewBook AddNewBook={AddNewBook} />
+      </>
+  );
+};
 
 export default Books;
