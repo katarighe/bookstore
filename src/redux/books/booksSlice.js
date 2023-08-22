67ea7e6aed5ac5153309849d7f2562624827727b
katarighe <m.ighe@hotmail.com>
2023-08-22 19:10:25 +0300

    Rename to BooksSlice and fix the errors

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -3,54 +3,45 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   books: [
     {
-      id: 1,
+      item_id: 'item1',
       title: 'The Hitchhiker`s Guide to the Galaxy',
       author: 'Douglas Adams',
       category: 'Science fiction',
     },
     {
-      id: 2,
+      item_id: 'item2',
       title: 'The Lord of the Rings',
       author: 'J.R.R. Tolkien',
       category: 'Fantasy',
     },
     {
-      id: 3,
+      item_id: 'item3',
       title: 'To Kill a Mockingbird',
       author: 'Harper Lee',
       category: 'Literary fiction',
     },
     {
-      id: 4,
+      item_id: 'item4',
       title: 'The Great Gatsby',
       author: 'F. Scott Fitzgerald',
       category: 'Classics',
     },
-    {
-      id: 5,
-      title: 'Pride and Prejudice',
-      author: 'Jane Austen',
-      category: 'Romance',
-    },
   ],
 };
 
-const booksSlice = createSlice({
+export const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
     addBook: (state, action) => {
-      const { title, author } = action.payload;
-      if (!title || !author) {
-        return;
-      }
-      state.books.push(action.payload);
+      state.value.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.books = state.books.filter((book) => book.id !== action.payload);
+      state.value = state.value.filter((book) => book.item_id !== action.payload);
     },
   },
 });
 
 export const { addBook, removeBook } = booksSlice.actions;
+
 export default booksSlice.reducer;
